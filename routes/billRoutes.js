41737f140dea4349c95c9bafddf1fee50ed15c0f
jeya-chandran-jcs <145828553+jeya-chandran-jcs@users.noreply.github.com>
2024-01-26 23:56:19 +0530

    Update billRoutes.js

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -23,5 +23,16 @@ router.get("/get-bill",async(req,res)=>{
    }
 })
 
+router.get("/get-one-bill", async (req, res) => {
+    try {
+        const userId = req.query.userId; // Assuming the user ID is passed as a query parameter
+        const bills = await billModel.find({ userId });
+
+        res.send(bills);
+    } catch (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 export default router
